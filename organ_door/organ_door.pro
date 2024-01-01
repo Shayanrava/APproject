9a568588c1a1320a12dc7964a23e0835f913e411
alirezahaddad123 <alirezahaddad039@gmail.com>
2024-01-01 19:24:37 +0330

    page of organization key confirm made

--- organ_door/organ_door.pro
+++ organ_door/organ_door.pro
@@ -9,17 +9,20 @@ CONFIG += c++17
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    key_pass_organ.cpp \
     main.cpp \
     mainwindow.cpp \
     making_decision_for_organ.cpp \
     organization_entrance.cpp
 
 HEADERS += \
+    key_pass_organ.h \
     mainwindow.h \
     making_decision_for_organ.h \
     organization_entrance.h
 
 FORMS += \
+    key_pass_organ.ui \
     mainwindow.ui \
     making_decision_for_organ.ui \
     organization_entrance.ui
--- organ_door/organization_entrance.cpp
+++ organ_door/organization_entrance.cpp
@@ -1,6 +1,7 @@
 #include "organization_entrance.h"
 #include "ui_organization_entrance.h"
 #include"making_decision_for_organ.h"
+#include"key_pass_organ.h"
 organization_entrance::organization_entrance(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::organization_entrance)
@@ -13,9 +14,16 @@ organization_entrance::~organization_entrance()
     delete ui;
 }
 
-void organization_entrance::on_pushButton_2_clicked()
+/*void organization_entrance::on_pushButton_2_clicked()
 {
     making_decision_for_organ *mdfo=new making_decision_for_organ();
     mdfo->show();
+}*/
+
+
+void organization_entrance::on_pushButton_2_clicked()
+{
+    key_pass_organ *kpo=new key_pass_organ();
+    kpo->show();
 }
 
