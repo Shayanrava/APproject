97d5bce209a873937b865c70243f30bfd5f18276
Shayan <shayanravakhah57@gmail.com>
2024-01-06 20:59:00 +0330

    signin.h complate and styling done

--- enter/signin.h
+++ enter/signin.h
@@ -2,6 +2,10 @@
 #define SIGNIN_H
 
 #include <QWidget>
+#include<QFile>
+#include<QTimer>
+#include"forget.h"
+
 
 namespace Ui {
 class signin;
@@ -12,11 +16,20 @@ class signin : public QWidget
     Q_OBJECT
 
 public:
+
     explicit signin(QWidget *parent = nullptr);
     ~signin();
+public slots:
+    void readinfo();
+    void closesignin();
 
 private:
+    QFile* file;
+    forget* forgetpassword;
+
     Ui::signin *ui;
+
+
 };
 
 #endif // SIGNIN_H
