799d2d98c87555f9bd9c78e6e4f04508dd9c8df1
Shayan <shayanravakhah57@gmail.com>
2024-01-10 17:05:22 +0330

    task complated

--- enter/task.cpp
+++ enter/task.cpp
@@ -2,5 +2,37 @@
 
 task::task()
 {
+    archive=true;
+    date_finish="";
+    task_work="";
 
+}
+void task::set_datefinis(QString time_finish){
+
+    date_finish=time_finish;
+
+}
+
+
+bool task::get_archive(){
+
+    return archive;
+}
+QString task::get_date_finish(){
+    return date_finish;
+}
+
+QString task::get_task(){
+    return task_work;
+}
+
+
+void task::set_taskwork(QString tasks){
+
+    task_work=tasks;
+}
+
+void task::set_archive(){
+
+  archive=!archive;
 }
--- enter/task.h
+++ enter/task.h
@@ -1,11 +1,26 @@
 #ifndef TASK_H
 #define TASK_H
 
+#include <QString>
+
 
 class task
 {
+
+    QString task_work;
+
+    bool archive;
+
+    QString date_finish;
+
 public:
     task();
+    void set_datefinis(QString );
+    void set_taskwork(QString);
+    void set_archive();
+    bool get_archive();
+    QString get_task();
+    QString get_date_finish();
 };
 
 #endif // TASK_H
