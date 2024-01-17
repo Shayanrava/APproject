6eb37e1bbf1c9ea6e95d12c25ce6c8b4f9545dde
alirezahaddad123 <alirezahaddad039@gmail.com>
2024-01-17 17:45:31 +0330

    get_name and remove and new_admin functions added to admins file

--- enter/admins.cpp
+++ enter/admins.cpp
@@ -1,2 +1,33 @@
 #include "admins.h"
 admins::admins() {}
+void admins::new_admin(QString Name)
+{
+    admins::admins_name.push_back(Name);
+}
+
+QString admins::get_name(int index)
+{
+    if(index>=admins_name.length()||index<0)
+    {
+        return "out of range index!";
+    }
+    return admins_name[index];
+}
+
+QString admins::remove(QString name)
+{
+    bool found=0;
+    for(int i=0;i<admins_name.length();i++)
+    {
+        if(admins_name[i]==name)
+        {
+            found==1;
+            admins_name.remove(i);
+            break;
+        }
+        if(i==admins_name.length()-1&&found==0)
+        {
+            return "No such person was found!";
+        }
+    }
+}
--- enter/admins.h
+++ enter/admins.h
@@ -1,11 +1,15 @@
 #ifndef ADMINS_H
 #define ADMINS_H
 #include<QString>
+#include<QVector>
 class admins
 {
+    QVector<QString>admins_name;
 public:
     admins();
-
+    void new_admin(QString);
+    QString get_name(int);
+    QString remove(QString);
 };
 
 #endif // ADMINS_H
