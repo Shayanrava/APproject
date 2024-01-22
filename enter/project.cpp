cb32969ea2f79571add00f70bed72932e7b27327
alirezahaddad123 <alirezahaddad039@gmail.com>
2024-01-22 13:05:11 +0330

    functions added to project.cpp

--- enter/project.cpp
+++ enter/project.cpp
@@ -8,3 +8,52 @@ project::project() {}
 //admins admins_of_projects;
 
 
+void project::setName(QString Name)
+{
+    projectName=Name;
+}
+
+QString project::getName()
+{
+    return projectName;
+}
+
+void project::setOrgan(QString Name)
+{
+    organName=Name;
+}
+
+QString project::getOrgan()
+{
+    return organName;
+}
+
+void project::setTask(QString TaskString)
+{
+    task_of_projects.set_taskwork(TaskString);
+}
+
+void project::setTaskTime(QString Time)
+{
+    task_of_projects.set_datefinis(Time);
+}
+
+void project::setTaskArchive()
+{
+    task_of_projects.set_archive();
+}
+
+QString project::getTaskTitle()
+{
+    return task_of_projects.get_task();
+}
+
+QString project::getTaskTime()
+{
+    return task_of_projects.get_date_finish();
+}
+
+bool project::getTaskArchive()
+{
+    return task_of_projects.get_archive();
+}
