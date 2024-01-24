312f4eb2d7497b7fdf2ffbfb5f1118b8d426763d
alirezahaddad123 <alirezahaddad039@gmail.com>
2024-01-24 11:49:09 +0330

    setter and getter for admin added

--- enter/organization.cpp
+++ enter/organization.cpp
@@ -5,3 +5,32 @@ organization::organization()
 {
 
 }
+
+void organization::addAdmin(QString Admin)
+{
+    organadmins.new_admin(Admin);
+}
+
+bool organization::removeAdmin(QString Admin)
+{
+    if(organadmins.remove(Admin)=="Admin removed successfully")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+QString organization::getAdmin(int Index)
+{
+    if(organadmins.get_name(Index)!="out of range index!")
+    {
+        return organadmins.get_name(Index);
+    }
+    else
+    {
+        return "out of range index!";
+    }
+}
--- enter/organization.h
+++ enter/organization.h
@@ -1,11 +1,19 @@
 #ifndef ORGANIZATION_H
 #define ORGANIZATION_H
-
-
+#include"project.h"
+#include"admins.h"
+#include"team.h"
 class organization
 {
+    QVector<project>organProjects;
+    QVector<team>organTeams;
+    admins organadmins;
+
 public:
     organization();
+
+    void addAdmin(QString);
+
 };
 
 #endif // ORGANIZATION_H
