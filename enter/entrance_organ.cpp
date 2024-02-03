0d61e91efcd5fda0613af7729532e0edc34e3e3e
Shayan <shayanravakhah57@gmail.com>
2024-02-03 18:20:37 +0330

    show by filter added

--- enter/entrance_organ.cpp
+++ enter/entrance_organ.cpp
@@ -43,8 +43,74 @@ void entrance_organ::on_pbn_removing_clicked()
 
 void entrance_organ::on_pbn_filter_clicked()
 {
-    filtering_organizations *fo=new filtering_organizations();
-    fo->show();
+    ui->textEdit_2->clear();
+
+   QString flt=ui->lineEdit->text();
+
+   QFile f(userName+".txt");
+
+   f.open(QIODevice::ReadOnly | QIODevice::Text);
+
+   QString useRead= f.readAll();
+
+   f.close();
+
+
+   QStringList listuse;
+
+   listuse=useRead.split("  ");
+
+
+   f.open(QIODevice::WriteOnly | QIODevice::Text);
+
+   f.resize(0);
+
+   QTextStream out(&f);
+
+   for(int i=0;i<listuse.length();i++){
+       if(listuse[i]==""){
+           continue;
+       }
+
+      out<<listuse[i]<<"  ";
+
+   }
+
+   f.close();
+
+   f.open(QIODevice::ReadOnly | QIODevice::Text);
+
+   QString useRead2= f.readAll();
+
+   f.close();
+
+
+   QStringList listuse2,listfil;
+
+   listuse2=useRead2.split("  ");
+
+  for(int i=0;i<listuse2.length();i+=5){
+
+      if(listuse2[i].contains(flt)){
+          qDebug()<<listuse2[i];
+
+          listfil.push_back(listuse2[i]);
+      }
+  }
+
+
+
+  for(int i=0;i<listfil.length();i++){
+
+      if(listfil[i]==""){
+          continue;
+      }
+
+  ui->textEdit_2->append(listfil[i]);
+
+  }
+
+
 }
 
 
